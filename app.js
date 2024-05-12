714bb18b1720b0edbef64f1e2ad534b00a630d66
ABBAASCH <theisabbaasch@gmail.com>
2024-05-12 18:35:29 +0200

    Updated comments in app.js for structure

--- app.js
+++ app.js
@@ -13,10 +13,11 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-
+// set html as file types in views dir
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
+// basic express js setup
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,13 +27,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Initialize session
 app.use(
   session({
-    secret: 'your-secret-key',
+    secret: 'verysecretkey',
     resave: false,
     saveUninitialized: false
   })
 );
 
+// router for guest views
 app.use('/', indexRouter);
+// router for user views
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
@@ -51,6 +54,8 @@ app.use(function(err, req, res) {
     res.render('error');
 });
 
+// start server on port 3000
+// url:  http://localhost:3000
 app.listen(3000, function() {
     console.log('Server started on port 3000');
 }
