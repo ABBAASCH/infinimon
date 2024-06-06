453103c6ed673ee9b1ba6f35d98fbfbdbc566f46
ABBAASCH <theisabbaasch@gmail.com>
2024-06-06 10:41:31 +0200

    Fixed the 'next function not defined' bug in app

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var createError = require('http-errors');
+const createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -9,7 +9,7 @@ const session = require('express-session');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -39,26 +39,28 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(next) {
+app.use(function(req, res, next) {
     next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res) {
+app.use(function(err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
+    res.render('error', {
+        message: err.message,
+        error: res.locals.error
+    });
 });
 
 // start server on port 3001
 // url:  http://localhost:3001
 app.listen(3001, function() {
     console.log('Server started on port 3001');
-}
-);
+});
 
 module.exports = app;
